9a1f7d2c778374645b3c406a7a2fe811b9c594aa
Terlibin Oleg <rumblefishinfo@gmail.com>
2023-12-07 16:26:16 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,105 @@
+#include <Arduino.h>
+#include <Stepper.h>
+#include <RtcDS1302.h>
+#include <ThreeWire.h>
+
+// Serial
+const int SERIAL_SPEED =       9600;
+// RA drive
+const int RA_DIRECTION_PIN =   4;
+const int RA_SPEED_PIN =       5;
+const int RA_GEAR_TRAIN =      2;
+const int RA_DEFAULT_RPM =     800;
+const int RA_MOTOR_STEPS =     800;
+const int RA_MOUNT_STEPS =     130;
+const int RA_MICRO_STEPS =     16;
+const int motorInterfaceType = 1;
+const int INTERATION_STEPS =   10000;
+// Watch
+const int  WATCH_CLK_PIN =     6;
+const int  WATCH_DAT_PIN =     7;
+const int  WATCH_RST_PIN =     8;
+const int HOURS_IN_SEC =       60 * 60;
+
+double stepsPerFullTurn = RA_MOUNT_STEPS * RA_MOTOR_STEPS * RA_GEAR_TRAIN * RA_MICRO_STEPS;
+double secondsForFullTurn = 24.00 * HOURS_IN_SEC;
+double secondsPerStep = secondsForFullTurn / stepsPerFullTurn;
+long positionTime = 0;
+
+ThreeWire myWire(WATCH_DAT_PIN, WATCH_CLK_PIN, WATCH_RST_PIN);
+RtcDS1302<ThreeWire> Rtc(myWire);
+Stepper raStepper(RA_MOTOR_STEPS, RA_DIRECTION_PIN, RA_SPEED_PIN);
+
+void updatePosition(long);
+long getLateTime();
+void move(long);
+long getStepsToMove(long);
+
+void setup()
+{ 
+  Serial.begin(SERIAL_SPEED);
+  Serial.println("Initialize ...");
+  // Watch initialize
+  Serial.print("Time module...");
+  Rtc.Begin();
+  RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+  Rtc.SetDateTime(compiled);
+  RtcDateTime startTime = Rtc.GetDateTime();
+  positionTime = startTime.Unix32Time();
+  Serial.println("Ok");
+  // RA drive initialize
+  Serial.print("Stepper module...");
+  raStepper.setSpeed(RA_DEFAULT_RPM);
+  Serial.println("Ok");
+  Serial.println("Initialized!");
+}
+
+void loop()
+{
+  long lateTime = getLateTime();
+  Serial.print("Late time: ");
+  Serial.println(lateTime);
+
+  updatePosition(lateTime);
+}
+
+long getLateTime()
+{
+  RtcDateTime now = Rtc.GetDateTime();
+  long latePerSeconds = now.Unix32Time() - positionTime;
+  positionTime = now.Unix32Time();
+
+  return latePerSeconds;
+}
+
+void updatePosition(long lateTime)
+{
+  if (lateTime > 0)
+  {
+    long steps = getStepsToMove(lateTime);
+    Serial.println("Moving... ");
+    Serial.print("Steps count: ");
+    Serial.println(steps);
+    move(steps);
+    Serial.println("move over");
+  }
+}
+
+long getStepsToMove(int sec)
+{
+  return sec / secondsPerStep;
+}
+
+void move(long steps)
+{
+  int index = abs(steps / HOURS_IN_SEC);
+  int remainderSteps = steps % HOURS_IN_SEC;
+
+  while (index > 0)
+  {
+    raStepper.step(HOURS_IN_SEC);
+    index--;
+  }
+
+  raStepper.step(remainderSteps);
+}
